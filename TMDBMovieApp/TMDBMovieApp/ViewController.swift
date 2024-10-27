92bbe3666e57f765c68f2432885a4494f3d8e111
Varga Szabolcs <vargaszabolcs@Vargas-MacBook-Pro.local>
2024-10-27 13:17:06 +0100

    Init project

---
+++ TMDBMovieApp/TMDBMovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TMDBMovieApp
+//
+//  Created by Varga Szabolcs on 2024. 10. 27..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
